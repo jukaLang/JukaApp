ff37bea9e0904e2c1d2d56fd7e9363b84fbe1784
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-01 23:28:42 -0400

    Partial Text Structure redesign: `ui` no `backend`

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -9,7 +9,9 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("ShiningHoovesRegular.ttf", "SHR");
+                fonts.AddFont("Architex.ttf", "Architex");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
