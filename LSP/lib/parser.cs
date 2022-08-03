76405cf2dd8620d60fa054524673854393cf802f
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-02 20:47:16 -0400

    added basic lib boiler for LSP parsing and reading

---
+++ LSP/lib/parser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JukaApp.LSP.lib
+{
+    internal class parser
+    {
+        public static void PARSER(string data)
+        {
+            var lines = data.Split(new string[]
+            {
+                Environment.NewLine
+            },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+
+        }
+    }
+}
