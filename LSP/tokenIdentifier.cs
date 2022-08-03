c7e8a4f5e6b89587bc0b191e52359dcb5b7507f2
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-02 20:40:05 -0400

    added language token identifiers

---
+++ LSP/tokenIdentifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JukaApp.LSP
+{
+    internal class TOKENS
+    {
+        public static string FUNCTION_DEFINE = "func";
+        public static string[] PRINT_STATEMENTS = { "printLine", "print" };
+        public static string VARIABLE_IDENTIFIER = "var";
+        public static string[] CONDITION_STATEMENTS = { "if", "else", "while", "break" };
+        public static string CLASS_IDENTIFIER = "class";
+    }
+}
