c206603760d0571c99c4c5d1994a4cc7110eb12a
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-02 20:33:44 -0400

    added LSP colour list

---
+++ LSP/syntax_colour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JukaApp.LSP
+{
+    public class COLOURING
+    {
+        public static string FUNCTION_NAME = "#5cb897";
+        public static string STRING_DATA = "#2a5e1d";
+        public static string BUILTIN_STATEMENT = "#7461b8";
+        public static string USER_STATEMENT = "#b8b661";
+        public static string VARIABLE_TYPE_IDENTIFIER = "#b861a6";
+        public static string VARIABLE_TYPE_NUMBER = "#61b88e";
+        public static string VARIABLE_TYPE_STRING = "#b86177";
+        public static string CONDITION_NAME = "#b8a161";
+        public static string LOOP_CALL = "#ac61b8";
+        public static string CLASS_IDENTIFIER = "#8b61b8";
+
+    }
+}
